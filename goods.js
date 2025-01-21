c58b2b50dccb4ce1428547fa91477e51cf1416cd
Templ08 <152804568+Templ08@users.noreply.github.com>
2025-01-21 17:47:56 +0500

    Add files via upload

---
+++ goods.js
@@ -0,0 +1,16 @@
+const GOODS = [{
+    name: 'Phantom Concord',
+    price: 60,
+	id: 1,
+	imgSrc: './копье.png',
+}, {
+	id: 2,
+	name: 'Concord Dominion',
+	price: 500,
+	imgSrc: './плечи.jpeg',
+}, {
+	id: 3,
+	name: 'Concord Reversion',
+	price: 2200,
+	imgSrc: './шляпа.jpeg',
+}];
